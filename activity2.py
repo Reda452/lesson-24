ab9cff2bfddeceedd490c838e06e3e6006f422f2
Reda452 <redasakhi0@gmail.com>
2025-02-18 19:26:44 +0100

    new1

---
+++ activity2.py
@@ -0,0 +1,23 @@
+# Zip elements of two lists
+s1 = ['1', '2', '3']
+s2 = {'a', 'b', 'c'}
+s3 = list(zip(s1, s2))
+print(s3, "\n")
+
+
+# zip elements of two lists
+# print elements one by one, but elements of 2nd list will be in reverse order
+list1 = [10, 20, 30, 40]
+list2 = [100, 200, 300, 400]
+
+for x, y in zip(list1, list2[::-1]):
+    print(x, y)
+
+
+# zip into dictionary
+stocks = ['relience', 'infosys', 'tcs']
+prices = [2175, 1127, 2750]
+
+new_dict = {stocks: prices for stocks,
+            prices in zip(stocks, prices)}
+print('\n{}'.format(new_dict))
